2afbc8da7efa6206caad3c24d216c8260ff72a9a
吴泓锐 <wuyuanrui@xiao100.com>
2020-04-22 18:04:34 +0800

    feat: 增加新的算法结题

--- js-algorithm-test/new/20200415/index.js
+++ js-algorithm-test/new/20200415/index.js
@@ -18,51 +18,51 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-  if(nums.length < 2){
+var search = function (nums, target) {
+  if (nums.length < 2) {
     return nums[0] === target ? 0 : -1
   }
   let start = 0
   let end = nums.length - 1
 
-  while(start <end){
+  while (start < end) {
     let mid = Math.floor((end - start) / 2) + start
     //取中值
-    if(nums[mid] === target) return mid
-    if(nums[start] === target) return start
-    if(nums[end] === target) return end
+    if (nums[mid] === target) return mid
+    if (nums[start] === target) return start
+    if (nums[end] === target) return end
 
     // 优化
-    if(nums[start] < nums[end]){
+    if (nums[start] < nums[end]) {
       // 如果右边的比左边大那么认为数组时正确排序了的 执行正产二分法
-      if(nums[mid] > target){
-        end = mid -1
-      }else {
+      if (nums[mid] > target) {
+        end = mid - 1
+      } else {
         start = start + 1
       }
       continue // 继续循环
     }
-    if(nums[mid] > nums[start]){
+    if (nums[mid] > nums[start]) {
       //左边是升序
-      if(target > nums[start] && target < nums[mid]){
+      if (target > nums[start] && target < nums[mid]) {
         // 只有target比左边大 比终点小才会在左边位置 不然肯定在右边
         end = mid - 1
-      }else {
+      } else {
         start = start + 1
       }
-    }else {
+    } else {
       // 右边是升序
-      if(target > nums[mid] && target < nums[end]){
+      if (target > nums[mid] && target < nums[end]) {
         // 右边是升序 只有大于终点 小于右边 target才有可能在右边 不然肯定在左边
         start = mid + 1
-      }else {
+      } else {
         end = mid - 1
       }
     }
   }
 
   return -1
-};
+}
 
 // 二叉树的最近公共祖先
 /*给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
@@ -90,8 +90,8 @@ var search = function(nums, target) {
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-var lowestCommonAncestor = function(root, p, q) {
-  if(root === null || root.val === q.val || root.val === p.val){
+var lowestCommonAncestor = function (root, p, q) {
+  if (root === null || root.val === q.val || root.val === p.val) {
     //找到了 直接返回 当前节点
     return root
   }
@@ -99,15 +99,91 @@ var lowestCommonAncestor = function(root, p, q) {
   const left = lowestCommonAncestor(root.left, p, q)
   const right = lowestCommonAncestor(root.right, p, q)
   // 如果都存在的话 那么当前root就是祖先
-  if(left && right){
+  if (left && right) {
     return root
   }
   // 如果都没有那么直接返回null
-  if(!left && !right){
+  if (!left && !right) {
     return null
   }
 
   // 一个有一个没有
   return left ? left : right
 
-};
+}
+
+//136. 只出现一次的数字
+//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+// 创建额外空间
+var singleNumber = function (nums) {
+  const map = {}
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]]) {
+      delete map[nums[i]]
+    } else {
+      map[nums[i]] = 1
+    }
+  }
+
+  return Object.keys(map)[0]
+}
+
+// 利用异或运算的性质 0^n = n  n^n=0 a^b=1
+// 异步运算符合交换律 即 a^b^c^a^b = a^a^b^b^c = 0^c = c
+var singleNumber2 = function (nums) {
+  let result = 0
+  for (let i = 0; i < nums.length; i++) {
+    result = result ^ nums[i]
+  }
+  return result
+  // return nums.reduce((prev,cur) => prev^cur)
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+
+// 块排
+  let left = 0
+  let right = nums.length - 1
+  let tempIndex = fastSort(nums, left, right)
+  while (k - 1 !== tempIndex) {
+    //继续递归
+    if (tempIndex > k - 1) {
+      //数字更大 数字在左边
+      right = tempIndex - 1
+
+    } else {
+      //在右边
+      left = tempIndex + 1
+    }
+    tempIndex = fastSort(nums, left, right)
+  }
+
+  // 当前元素就是第k个最大的元素了
+
+  return nums[k - 1]
+
+}
+
+function fastSort (nums, left, right) {
+  var pivot = nums[left]
+  while (left < right) {
+    while (left < right && nums[right] <= pivot) {
+      right--
+    }
+    nums[left] = nums[right]
+
+    while (left < right && nums[left] > pivot) {
+      left++
+    }
+    nums[right] = nums[left]
+  }
+  nums[left] = pivot
+  return left
+}
